e5994453cb045369dc8a811c1dc91f8103ef23a7
ViniciusFelinto <salesvinicius@id.uff.br>
2025-11-30 10:16:06 -0300

    Config Inicial do Banco e Arquivo

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import path from 'path';
-
-export default defineConfig({
-  // Mapeia a pasta 'data' como a pasta pública para assets (arquivos .parquet)
-  publicDir: path.resolve(__dirname, './data'), 
-});
